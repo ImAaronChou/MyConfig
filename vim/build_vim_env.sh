62a335cb709202ac918f77ec88c60e614023efe4
aaron <im.aaron.chou@gmail.com>
2021-11-20 14:15:56 +0800

    添加vim脚本

---
+++ vim/build_vim_env.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/zsh
+
+rm tmp_vim_env_Dockfile
+cp ./Dockerfile ./tmp_vim_env_Dockfile
+
+#设置权限
+cat << EOF >> ./tmp_vim_env_Dockfile
+RUN mkdir -p ${HOME} && \\
+    echo "${USER}:x:${UID}:${GID}::${HOME}:/bin/bash" >> /etc/passwd && \\
+    echo "${GROUP}:x:${GID}:" >> /etc/group && \\
+    mkdir -p /etc/sudoers.d && \\
+    echo "${USER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER} && \\
+    chmod 0440 /etc/sudoers.d/${USER} && \\
+    chown ${UID}:${GID} -R ${HOME} && \\
+    echo "${USER}:${USER}" | chpasswd && \\
+    echo "export DISPLAY=${DISPLAY}" >> /etc/profile
+EOF
+
+docker build . -t vim_env -f ./tmp_vim_env_Dockfile
+rm tmp_vim_env_Dockfile
+cp ./vim ~/.local/bin
+
+
