af3ba252624cadf2d594470ff478e9519f809045
aaron <im.aaron.chou@gmail.com>
2022-01-25 12:21:57 +0800

    更新

--- vim/build_vim_env_20_04.sh
+++ vim/build_vim_env_20_04.sh
@@ -5,17 +5,17 @@ rm tmp_vim_env_Dockfile
 cp ./Dockerfile_20_04 ./tmp_vim_env_Dockfile
 
 #设置权限
-#cat << EOF >> ./tmp_vim_env_Dockfile
-#RUN mkdir -p ${HOME} && \\
-#    echo "${USER}:x:${UID}:${GID}::${HOME}:/bin/bash" >> /etc/passwd && \\
-#    echo "${GROUP}:x:${GID}:" >> /etc/group && \\
-#    mkdir -p /etc/sudoers.d && \\
-#    echo "${USER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER} && \\
-#    chmod 0440 /etc/sudoers.d/${USER} && \\
-#    chown ${UID}:${GID} -R ${HOME} && \\
-#    echo "${USER}:${USER}" | chpasswd && \\
-#    echo "export DISPLAY=${DISPLAY}" >> /etc/profile
-#EOF
+cat << EOF >> ./tmp_vim_env_Dockfile
+RUN mkdir -p ${HOME} && \\
+    echo "${USER}:x:${UID}:${GID}::${HOME}:/bin/bash" >> /etc/passwd && \\
+    echo "${GROUP}:x:${GID}:" >> /etc/group && \\
+    mkdir -p /etc/sudoers.d && \\
+    echo "${USER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER} && \\
+    chmod 0440 /etc/sudoers.d/${USER} && \\
+    chown ${UID}:${GID} -R ${HOME} && \\
+    echo "${USER}:${USER}" | chpasswd && \\
+    echo "export DISPLAY=${DISPLAY}" >> /etc/profile
+EOF
 
 docker build . --build-arg REBUILD_VAR=$(date +%Y%m%d-%H%M%S) -t vim_env -f ./tmp_vim_env_Dockfile
 rm tmp_vim_env_Dockfile
